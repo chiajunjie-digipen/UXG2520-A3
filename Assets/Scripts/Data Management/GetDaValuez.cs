4a450a2cf51a6e4dec754e00627edd653e53f274
chiajunjie-digipen <122497239+chiajunjie-digipen@users.noreply.github.com>
2023-07-12 18:08:01 +0800

    working on csv reader

--- Assets/Scripts/Data Management/GetDaValuez.cs
+++ Assets/Scripts/Data Management/GetDaValuez.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class GetDaValuez : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    private string data = "";
     void Start()
     {
+        StreamReader reader = new StreamReader("Assets/CSVs/itemEffects.csv");
         
+        while (data != null){
+            data = reader.ReadLine();
+            Debug.Log(data.Split(',')[0]);
+        }
+        
+        reader.Close();
     }
 
     // Update is called once per frame
